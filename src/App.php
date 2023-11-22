33ef922ed18d061a6ecd12351b213c43da89c7ce
tarari <t.jimenez@escolesnuria.cat>
2023-11-22 19:32:38 +0100

    App dispatch

--- src/App.php
+++ src/App.php
@@ -12,8 +12,33 @@
         {
             $this->request=new Request;
             //$request nos ofrece api o controlador y accion
-            echo $this->request->getController();
-            echo $this->request->getAction();
+            $controller=$this->request->getController();
+            $action=$this->request->getAction();
+            $this->dispatch($controller,$action);
         }
         
+        private function dispatch($controller,$action){
+          try{
+              $nameController='\\App\Controllers\\'.ucfirst($controller).'Controller';
+              $objContr=new $nameController();
+              if(is_callable([$objContr,$action])){
+                  call_user_func([$objContr,$action]);
+              }else{
+                call_user_func([$objContr,'error']);
+              }
+          }catch(\Exception $e){
+            die($e->getMessage());
+          }
+        }
+        private function getRoutes():array{
+          $routes=[];
+          $dir=__DIR__.'/Controllers';
+          $handle=opendir($dir);
+          while(($entry=readdir($handle))!=false){
+            if($entry!='.' && $entry!='..'){
+              $routes[]=strtolower(substr($entry,0,-14));
+            }
+          }
+          return $routes;
+        }
     }
