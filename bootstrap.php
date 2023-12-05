c44c75107688cc5587e2c550d1f47ed4671c44a2
tarari <t.jimenez@escolesnuria.cat>
2023-12-05 16:31:25 +0100

    Registry && db

--- bootstrap.php
+++ bootstrap.php
@@ -2,3 +2,16 @@
 
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->safeLoad();
+
+    use App\Database\QueryBuilder;
+    use App\Database\Connection;
+    use App\Registry;
+    // register all the services
+    
+
+    Registry::bind('config', require 'config.php');
+    
+    Registry::bind('database', new QueryBuilder(
+        Connection::make(Registry::get('config'))
+    ));
+    
