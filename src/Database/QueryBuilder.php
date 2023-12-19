de8e7d5ef59ae919ca04f38407ec64a0b8295b7f
tarari <t.jimenez@escolesnuria.cat>
2023-12-19 14:43:16 +0100

    QueryBuilder with Active Record

--- src/Database/QueryBuilder.php
+++ src/Database/QueryBuilder.php
@@ -16,11 +16,74 @@ class QueryBuilder{
     function selectAll($table){
         $statement = $this->pdo->prepare("select * from {$table}");
         $statement->execute();
-        return $statement->fetchAll(\PDO::FETCH_CLASS);
+        return $statement->fetchAll();
     }
 
-    public function selec(){
-        $this->selectables=func_get_args();
-        return $this;
+    public function insert(string $table, array $data): bool {
+        $fields = implode(', ', array_keys($data));
+        $placeholders = ':' . implode(', :', array_keys($data));
+        
+        $query = "INSERT INTO $table ($fields) VALUES ($placeholders)";
+        $statement = $this->pdo->prepare($query);
+
+        return $statement->execute($data);
+    }
+
+    public function select(string $table, array $conditions = [], array $fields = ['*']): array {
+        $fields = implode(', ', $fields);
+        $query = "SELECT $fields FROM $table";
+
+        if (!empty($conditions)) {
+            $query .= " WHERE ";
+            $conditionsStrings = [];
+            foreach ($conditions as $field => $value) {
+                $conditionsStrings[] = "$field = :$field";
+            }
+            $query .= implode(' AND ', $conditionsStrings);
+        }
+
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($conditions);
+
+        return $statement->fetchAll();
+    }
+
+    public function delete(string $table, array $conditions): int {
+        $query = "DELETE FROM $table WHERE ";
+        $conditionsStrings = [];
+        foreach ($conditions as $field => $value) {
+            $conditionsStrings[] = "$field = :$field";
+        }
+        $query .= implode(' AND ', $conditionsStrings);
+
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($conditions);
+
+        return $statement->rowCount();
+    }
+
+    public function update(string $table, array $data, array $conditions): int {
+        $fields = [];
+        foreach ($data as $field => $value) {
+            $fields[] = "$field = :$field";
+        }
+        $fields = implode(', ', $fields);
+
+        $query = "UPDATE $table SET $fields WHERE ";
+        $conditionsStrings = [];
+        foreach ($conditions as $field => $value) {
+            $conditionsStrings[] = "$field = :$field";
+        }
+        $query .= implode(' AND ', $conditionsStrings);
+
+        $statement = $this->pdo->prepare($query);
+        $mergedData = array_merge($data, $conditions);
+        $statement->execute($mergedData);
+
+        return $statement->rowCount();
+    }
+
+    public function findById(string $table, int $id): ?array {
+        return $this->select($table, ['id' => $id])[0] ?? null;
     }
 }
